d2fb95a71752d56c5983565d6b61f75dc9a20a07
Laura Asmakovska <laura.asmakovska@gmail.com>
2022-04-04 03:55:51 +0300

    Nim game v2.0

--- MiniMaxAlgorithmNim.cs
+++ MiniMaxAlgorithmNim.cs
@@ -20,13 +20,12 @@ public int[] stickPiles;
 public int btnID;
 bool maximizing;
 public int[] pcMoveValues;
-public int allSticks;
 public bool onePileTaken;
 bool isMinLayer;
 public Image[] stickOne;
 public Image[] stickTwo;
 public Image[] stickThree;
-
+public int allSticks;
     void Start(){
         for (int i = 0; i < buttons.Length; i++){
             int indexX = i;
@@ -44,10 +43,10 @@ public Image[] stickThree;
     }
 
     void setVariables(){
-        isMinLayer = false;
+        isMinLayer = true;
         onePileTaken = false;
         stickPiles = new int[3] {3,2,1};
-        allSticks = countSticks(stickPiles);
+        int allSticks = countSticks(stickPiles);
         whoWonTextTMP.enabled = false;
         maximizing = true;
         nextTurnBtn.interactable = false;
@@ -68,14 +67,15 @@ public Image[] stickThree;
         }
 
         if(GameStart.firstTurn == "Human"){
-            turnText = "Human";
+            turnText = "You start first!";
         }else{
-            turnText = "AI";
+            turnText = "Computer has started first!";
+            maximizing = !maximizing;
             computerMove();
         }
         
-        turnTextTMP.text = "Turn: " + turnText;
-        amountTextTMP.text = "Amount of sticks in a heap: " + allSticks;
+        turnTextTMP.text = turnText;
+        updateInfo();
 
     }
 
@@ -83,21 +83,8 @@ public Image[] stickThree;
         int stickThreeLength = 0;
         int stickTwoLength = 0;
         int stickOneLength = 0;
-        allSticks = countSticks(stickPiles);
-        if (maximizing == true){
-            if(GameStart.firstTurn == "Human"){
-                turnText = "Human";
-            }else{
-                turnText = "AI";
-            }
-        }
-        else{
-            if(GameStart.firstTurn == "AI"){
-                turnText = "AI";
-            }else{
-                turnText = "Human"; 
-            }
-        }
+        int allSticks = countSticks(stickPiles);
+
 
         for (int i = 0; i < stickThree.Length; i++){
             if(stickThree[i].enabled == true){
@@ -130,34 +117,37 @@ public Image[] stickThree;
             }
         }
 
-        turnTextTMP.text = "Turn: " + turnText;
         amountTextTMP.text = "Amount of sticks in a heap: " + allSticks;
     }
 
     void Update(){
+    if(gameOver()){
+        if(maximizing == true && GameStart.firstTurn == "PC" || maximizing == false && GameStart.firstTurn == "Human"){
+            nextTurnBtn.interactable = false;
+            whoWonTextTMP.enabled = true;
+            whoWonTextTMP.text = "PC has won!";
+        }else if (maximizing == false && GameStart.firstTurn == "PC" || maximizing == true && GameStart.firstTurn == "Human") {
+            nextTurnBtn.interactable = false;
+            whoWonTextTMP.enabled = true;
+            whoWonTextTMP.text = "You won!";
+        }
+    }
+    
         if(maximizing == false){
             computerMove();
             if(!gameOver()){
                 nextTurn();
-            }else{
-                nextTurnBtn.interactable = false;
-                whoWonTextTMP.enabled = true;
-                whoWonTextTMP.text = "PC has won!";
-            }
-        }else{
-            if(gameOver()){
-                nextTurnBtn.interactable = false;
-                whoWonTextTMP.enabled = true;
-                whoWonTextTMP.text = "You won!";
-            }
+            } 
         }
+
     }
 
     public void playerMove(int btnID){
         nextTurnBtn.interactable = true;
+        //atjauno gājiena mainīgo
         onePileTaken = false;
+        //pārbauda, kuras kaudzes poga tika nospiesta un attiecīgi veic izmaiņas spēles laikumā un tā grafiskos komponentos
         if(btnID == 0){
-
             if (stickPiles[0] != 0){
                 stickPiles[0] = stickPiles[0] - 1;
                 buttons[1].GetComponent<Button>().interactable = false;
@@ -186,20 +176,27 @@ public Image[] stickThree;
                 }
             }
         }
+        //atjauno spēles informāciju
         updateInfo();
     }
 
     public void computerMove(){
-        int[] chosenMove = evaluatePCMove(stickPiles);
+        //piešķir datora gājienu pie mainīga
+        int[] chosenMove = chooseComputerMove(stickPiles);
+        //pārbauda, lai gājieni ir derīgi (> 0)
         if (chosenMove[0] > 0 || chosenMove[1] > 0){
+            //pielieto gājienu vērtības spēles laukumā
             stickPiles[chosenMove[0]] = stickPiles[chosenMove[0]] - chosenMove[1];
+            //padara gājienu pogas neaktīvas, ja kaudze ir = 0
             for (int k = 0; k < stickPiles.Length; k++){
                 if(stickPiles[k] == 0){
                     buttons[k].GetComponent<Button>().interactable = false;
                 }
             }
+            //mainīgais, kas rāda, ka dators jau ir veicis gajienu
             onePileTaken = true;
             nextTurnBtn.interactable = true;
+            //atjauno saskarnes informāciju par spēles laukumu
             updateInfo();
         }
     }
@@ -224,52 +221,96 @@ public Image[] stickThree;
         }
     }
     
+    //heiristiska novertejuma funkcija
     public int toScore(int[] stickPiles){
+        //hieristiskais novērtējums
+        int totalScoreValue = 0;
+
+        //cikls, lai atrastu num summu
+        int nimSum = stickPiles[0];
+        for (int i = 1; i < stickPiles.Length; i++)
+            nimSum ^= stickPiles[i];
+
+        //novērtējums virstonēm, izmantojot šīs virsotnes nim summu
+            //papildnosācījums, lai nesajauktu nimSum = 0 ar virsotni, kas vienmēr noved pie zaudējuma (vai uzvaras), kad kaudzes konfigurācija ir (1 1 0, 1 0 1, 0 1 1)
+            if(nimSum == 0 && countSticks(stickPiles) != 2){
+                if(isMinLayer == false){
+                    totalScoreValue = totalScoreValue - 50;
+                }else if (isMinLayer == true){
+                    totalScoreValue = totalScoreValue + 50;
+                }
+            //papildnosācījums, lai nesajauktu nimSum = 1 ar uzvaras virsotni
+            }else if (nimSum == 1 && countSticks(stickPiles) > 1){
+                if(isMinLayer == false){
+                    totalScoreValue = totalScoreValue - 25;
+                }else if (isMinLayer == true){
+                    totalScoreValue = totalScoreValue + 25;
+                }
+            }else if (nimSum > 1){
+                if(isMinLayer == false){
+                    totalScoreValue = totalScoreValue - 5;
+                }else if (isMinLayer == true){
+                    totalScoreValue = totalScoreValue + 5;
+                }
+            }
+        
+            //novērtējums strupceļa virstonēm
              if(stickPiles[0] == 1 && stickPiles[1] == 0 && stickPiles[2] == 0){
                 if(isMinLayer == true){
-                    return 200;
+                   totalScoreValue = totalScoreValue + 200;
                 }else if (isMinLayer == false){
-                    return -200;
+                    totalScoreValue = totalScoreValue - 200;
                 }
             }
 
             if(stickPiles[1] == 1 && stickPiles[0] == 0 && stickPiles[2] == 0){
                 if(isMinLayer == true){
-                    return 200;
+                    totalScoreValue = totalScoreValue + 200;
                 }else if (isMinLayer == false){
-                    return -200;
+                   totalScoreValue = totalScoreValue - 200;
                 }
             }
 
             if(stickPiles[2] == 1 && stickPiles[0] == 0 && stickPiles[1] == 0){
                 if(isMinLayer == true){
-                    return 200;
+                    totalScoreValue = totalScoreValue + 200;
                 }else if (isMinLayer == false){
-                    return -200;
+                    totalScoreValue = totalScoreValue - 200;
                 }
             }
- 
-        return 0;
+
+        return totalScoreValue;
     }
     
     //minimaksa algoritms
     public int minimax(int[] stickPiles, int depth, bool maximizing){
-        isMinLayer = !isMinLayer;
-        //novertejuma funkcija (kurs speletajs uzvares - HUMAN vai PC)
+        //heiristiska novertejuma funkcija
         int scoreNow = toScore(stickPiles);
+        
+        //ja ir strupceļa virstone, beidz algoritmu un atgriež vērtību
         if (scoreNow == 200) return scoreNow;
         if (scoreNow == -200) return scoreNow;
+        //ja dziļums ir 0, tad beidz algoritmu - t.i. neturpina meklēt vērtības dziļākā koka līmēnī un izdod labāko vertību no šī līmeņa
+        if (depth == 0) return scoreNow;
+        
+        //līmeņu pārslēgšana (ir min vai max spēlētāja līmenis)
+        isMinLayer = !isMinLayer;
 
-        //MAX speletaja gajiens
+        //MAX speletaja  gajiens
         if (maximizing){
             int bestScore = -500;
             for (int k = stickPiles.Length - 1; k >= 0; k--){
                 //var iznemt no 1 lidz 3 objektiem no rindas
                 for(int z = 3; z > 0; z--){
+                    //objektu skaits kaudzē nedrīkst būt zemāks par izņemto objektu skaitu
                     if (stickPiles[k] >= z) {
+                        //tiek veikts gājiens
                         stickPiles[k] = stickPiles[k] - z;
+                        //gājiens tiek novērtēts
                         scoreNow = minimax(stickPiles, depth + 1, false);
+                        //tiek saglābāta vislābāka gājiena vērtība
                         bestScore = Math.Max(bestScore, scoreNow);
+                        //gājiens tiek atgriezts (lai spēles laukums nemainītos)
                         stickPiles[k] = stickPiles[k] + z;
                     }
                 }
@@ -293,33 +334,43 @@ public Image[] stickThree;
     }
 
     //datora gajiena funkcija
-    public int[] evaluatePCMove(int[] stickPiles){
+    public int[] chooseComputerMove(int[] stickPiles){
+        //tiek definēts vislabāka gājiena novērtējuma mainīgais
         int bestMoveValue = -500;
+        //tiek definēts vislabāka gājiena mainīgais
         int[] pcMoveValues = new int[2] {-1, -1};
+        //tiek saskaitīti visi objekti kaudzē
         int stickCountNow = countSticks(stickPiles);
-        for (int i = stickPiles.Length - 1; i >= 0; i--){
+        for (int i =  stickPiles.Length - 1; i >= 0; i--){
             for(int j = 3; j > 0; j--){
+                //tiek pārbaudīts, vai dators jau nav veicis gājienu
                 if (stickPiles[i] >= j && onePileTaken == false){
+                    //tiek pārbaudīts, vai visu objektu skaits nav vienāds ar izņēmto objektu skaitu, lai beigās objektu skaits nebūtu 0
                     if(j != countSticks(stickPiles)){
+                        //tiek veikts gājiens
                         stickPiles[i] = stickPiles[i] - j;
+                        //tiek veikts gājiena novērtējums
                         int currentMove = minimax(stickPiles, 0, false);
+                        //tiek atgriezts gājiens
                         stickPiles[i] = stickPiles[i] + j;
-                        if(currentMove > bestMoveValue){
+                        //Debug.Log("Current move: " + currentMove + " best move " + bestMoveValue);
+                        //tiek salīdzināta tagadēja gājiena vērtība ar vislabāko gājiena vērtību
+                        if(currentMove >= bestMoveValue){
+                            //tiek piešķirtas gājiena vērtības, kur i ir kaudzes numurs un j ir objektu skaits, kas tiks noņemts no kaudzes
                             pcMoveValues[0] = i;
                             pcMoveValues[1] = j;
                             bestMoveValue = currentMove;
                         }   
-                        //Debug.Log(currentMove + "<- current move value. && best Value ->" + bestMoveValue);
                     }
                 }
             }
         }
-        
+        //tiek atgrieztas gājiena vērtības, lai veiktu gājienu
         return pcMoveValues;
     }
 
     public bool gameOver(){
-        if (allSticks > 1){
+        if (countSticks(stickPiles) > 1){
             return false;
         }else{
             disableButtons();
